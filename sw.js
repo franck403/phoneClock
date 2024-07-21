cdad7c4fc78de8f4d73156d9fe0dbd66e4e57df3
franck403 <79372051+franck403@users.noreply.github.com>
2024-07-21 08:18:27 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,15 +6,25 @@ const URLS_TO_CACHE = [
   '/styles.css',
   '/offline.html'
 ];
-
+const externalAssets = []
+const externalAssetsURl = []
 // Install event listener: Cache the static resources during installation
 self.addEventListener('install', event => {
+    event.waitUntil(
+    caches.open('external-assets').then(cache => {
+      // cache the external-assets
+      return cache.addAll([
+        'https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css'
+      ]);
+    })
+  );
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(URLS_TO_CACHE);
       })
   );
+  
 });
 
 // Fetch event listener: Handle requests based on network availability
@@ -22,7 +32,13 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     // Try to fetch the content from the server
     fetch(event.request).catch(() => {
-      // If fetch fails (user is offline), look for a cached version
+      if (event.request.url.includes('/externalAsset/boxicons.min.css')) {
+          event.respondWith(
+            caches.match('https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css').then(response => {
+              return response || fetch(event.request);
+            })
+          );
+      }
       return caches.match(event.request).then(response => {
         if (response) {
           return caches.match('/offline.html');
